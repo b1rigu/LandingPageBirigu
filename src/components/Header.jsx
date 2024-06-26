7ec4249f8291c8f75cb59d1982ae1eb38ec8d53e
b1rigu <gbilly2013@gmail.com>
2024-06-26 14:49:05 +0900

    added resume link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,14 @@ export default function Header() {
   return (
     <header>
       <GithubLogo />
-      <button className="btn btn-outline btn-primary">My resume</button>
+      <a
+        className="btn btn-outline btn-primary"
+        target="_blank"
+        rel="nofollow"
+        href="https://www.upwork.com/freelancers/~0117d745f818e520d1"
+      >
+        My resume
+      </a>
     </header>
   );
 }
