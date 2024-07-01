0955b7f4a3cb0aecefb7489608d0ed5af3b50ac8
b1rigu <gbilly2013@gmail.com>
2024-07-01 11:45:38 +0900

    added reveal to experience card

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,4 +1,5 @@
 import TechPill from "./TechPill";
+import Reveal from "./Reveal";
 
 export default function ExperienceCard({
   company,
@@ -12,17 +13,29 @@ export default function ExperienceCard({
   return (
     <div className="mb-8">
       <div className="flex justify-between mb-4">
-        <p className="font-bold text-2xl">{company}</p>
-        <p>
-          {startDate} - {endDate}
-        </p>
+        <Reveal>
+          <p className="font-bold text-2xl">{company}</p>
+        </Reveal>
+        <Reveal>
+          <p>
+            {startDate} - {endDate}
+          </p>
+        </Reveal>
       </div>
       <div className="flex justify-between mb-4">
-        <p className="font-bold text-xl text-primary-color">{position}</p>
-        <p>{location}</p>
+        <Reveal>
+          <p className="font-bold text-xl text-primary-color">{position}</p>
+        </Reveal>
+        <Reveal>
+          <p>{location}</p>
+        </Reveal>
       </div>
-      <p className="mb-4">{description}</p>
-      <TechPill className="mb-4" items={techsUsed} />
+      <Reveal>
+        <p className="mb-4">{description}</p>
+      </Reveal>
+      <Reveal>
+        <TechPill className="mb-4" items={techsUsed} />
+      </Reveal>
       <hr className="opacity-20"></hr>
     </div>
   );
